af0640d7938426cd15897022e270bb0f3d88ab6d
CosmWatch <cosmwatch@gmail.com>
2023-02-25 21:21:55 -0500

    Add cluster 2-3 exercise and an easy reference to cw-plus

---
+++ Exercises/Exercise-W4-D3/simple-actor/src/contract.rs
@@ -0,0 +1,49 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+use cw20_base::contract::instantiate as cw20_base_instantiate;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:simple-actor";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    // CW2 migrate info
+    // set_contract_version(deps.storage, "", version);
+
+    // Setup TOKENS state if needed
+    // Instantiate CW20
+    cw20_base_instantiate(deps, env, info, msg);
+
+    let msg = ;
+
+    Response::new().add_message(msg);
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ Exercises/Exercise-W4-D3/simple-actor/src/state.rs
@@ -0,0 +1,4 @@
+use cosmwasm_schema::cw_serde;
+use cw_storage_plus::{Item, Map};
+
+// const TOKENS = Item()
