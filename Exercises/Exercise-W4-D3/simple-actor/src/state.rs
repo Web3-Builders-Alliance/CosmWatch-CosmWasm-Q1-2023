5d365def2adf200895fc4574dfeb56d334a5502c
CosmWatch <cosmwatch@gmail.com>
2023-03-02 09:14:24 -0500

    Upload classwork

--- Exercises/Exercise-W4-D3/simple-actor/src/state.rs
+++ Exercises/Exercise-W4-D3/simple-actor/src/state.rs
@@ -1,4 +1,12 @@
 use cosmwasm_schema::cw_serde;
-use cw_storage_plus::{Item, Map};
+use cosmwasm_std::{Addr, Coin};
+use cw_storage_plus::Item;
 
-// const TOKENS = Item()
+pub const LP_TOKEN: Item<Coin> = Item::new("lp_token");
+pub const CONFIG: Item<Config> = Item::new("config");
+
+#[cw_serde]
+pub struct Config {
+    pub cw20_address: Addr,
+    pub owner: Addr,
+}
